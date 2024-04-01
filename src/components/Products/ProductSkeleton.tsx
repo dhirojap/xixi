d70f1943c5f72b488edc539803a2c5bc56e73959
DhiroJap <dhirojap@gmail.com>
2024-04-02 00:32:10 +0700

    Landing Page + Loading State

---
+++ src/components/Products/ProductSkeleton.tsx
@@ -0,0 +1,14 @@
+const ProductSkeleton = () => {
+  return (
+    <div className="relative animate-pulse">
+      <div className="aspect-square w-full overflow-hidden lg:aspect-none lg:h-80">
+        <div className="h-full w-full bg-gray-200" />
+      </div>
+      <div className="mt-4 flex flex-col gap-2">
+        <div className="bg-gray-200 h-4 w-full" />
+        <div className="bg-gray-200 h-4 w-full" />
+      </div>
+    </div>
+  );
+};
+export default ProductSkeleton;
